7493ee7562bafc0097ec324998df751f41f7297e
Rishabh Tater <84710834+romaanchak-Rishabh@users.noreply.github.com>
2021-06-28 11:51:04 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a pulvinar purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    </div>
+  );
+}
+
+export default Info;
